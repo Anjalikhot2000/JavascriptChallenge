ed718dd98d3940c55ba768a12d8d99cc085859c9
Anjali Khot <anjalikhot5268@gmail.com>
2024-09-25 19:07:34 +0530

    fetchapi
    fetchapi

--- 11_fetchapi/_1fectchapi.js
+++ 11_fetchapi/_1fectchapi.js
@@ -1,8 +1,14 @@
-const URL= "https://dog.ceo/api/breeds/image/random"
-
+const URL= "https://catfact.ninja/fact"
+let factpara=document.querySelector("#fact")
+let btn=document.querySelector("#btn")
 
 const getData= async()=>{
     console.log("getting data...")
     let response= await fetch(URL)
-    console.log(response)
+    console.log(response) //json format 
+    let data=  await response.json()
+    console.log(data)
+    factpara.innerText=data.fact
 }
+
+btn.addEventListener("click",getData)
